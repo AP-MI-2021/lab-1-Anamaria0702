7e4cdb83a7ce7d056bea29fc5a17e7890b0e221b
Anamaria0702 <gavrilescuanamaria02@yahoo.com>
2021-10-06 12:01:54 +0300

    tema

--- main.py
+++ main.py
@@ -3,21 +3,32 @@ Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
   # codul vostru aici
-  return 45
-  
+  if (n==0 or n==1):
+    return False
+  for d in range(2, n//2):
+      if (n%d==0):
+          return False
+  return True    
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
   # codul vostru aici
-  pass
+  prod=1
+  for el in range(0, len(lst)):
+      prod= prod * lst[el]
+  return prod
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
   # codul vostru aici
-  pass
+  while x!=y:
+      if x>y: x=x-y
+      else: y=y-x
+  print("cmmdc=",x)
+  
   
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
@@ -28,6 +39,33 @@ def get_cmmdc_v2(x, y):
   
 def main():
   # interfata de tip consola aici
-  print("text")
+  print('''
+    1. IS prime
+    2. Product
+    3. Cmmdc v1
+    4. Cmmdc v2
+  ''')
+  a=int(input("Comanda:"))
+  if(a==1):
+      #is prime
+      n=int(input("Introduceti n="))
+      print(is_prime(n))
+  if(a==2):
+      #product
+      n=int(input("Introduceti n="))
+      list=[]
+      for i in range(0,n):
+          el=int(input())
+          list.append(el)
+      print(list)
+      print(get_product(list))
+  if(a==3):
+      #cmmdc v1
+      x=int(input("x="))
+      y=int(input("y="))
+      get_cmmdc_v1(x, y)
+      
+
+
 if __name__ == '__main__':
-  main()
+    main()
